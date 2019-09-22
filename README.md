b48afa1956b0645a3ceecde903078e8f8df20f55
lane <huulane@gmail.com>
2019-09-22 17:17:43 +0800

    Done

---
+++ README.md
@@ -0,0 +1,14 @@
+# crawler demo
+
+## idea
+
+Prepare the links.
+Download them & save them as local files.
+Observe the page & preapare the element selector.
+Use third-party package to parse the HTML & get you-wanted data.
+Josnify the data and save to local.
+
+## demo
+
+[simple](kuwo.py)
+[complicated](xiaozhu.py)
---
+++ kuwo.py
@@ -0,0 +1,23 @@
+from utils import *
+import requests
+from pyquery import PyQuery as pq
+
+
+def main():
+    link = 'https://www.kugou.com/yy/rank/home/1-8888.html?from=rank'
+    d = pq(requests.get(link).text)
+    selector = '#rankWrap > div.pc_temp_songlist > ul > li:nth-child({}) > a'
+
+    def get_info(i):
+        t = d(selector.format(i)).text()
+        return dict(
+            rank=i,
+            title=t,
+        )
+
+    data = list(map(get_info, range(1, 22+1)))
+    save_jsonify(data, './kugou.json')
+
+
+if __name__ == "__main__":
+    main()
